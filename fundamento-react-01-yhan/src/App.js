56593010ef61737c7a6b52a340234b2dbf68d39a
YhanMassato <yhanashihara@gmail.com>
2024-02-15 22:23:22 -0300

    Yhan:capa do livro adicionada sem fazer uso do css

--- fundamento-react-01-yhan/src/App.js
+++ fundamento-react-01-yhan/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import cavernas from './livros/cavernas_aco.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>As cavernas de aço</h3>
+
+      <p>Isaac Asimov</p>
+
+      <img src={cavernas} alt='capa do livro cavernas de aço por Isaac Asimov' style={{width:'300px'}}></img>
+
+      <p>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclusive nos dramas policiais.</p>
     </div>
   );
 }
