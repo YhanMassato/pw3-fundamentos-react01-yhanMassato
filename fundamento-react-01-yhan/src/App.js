2c432e07332669946a9be772ccdacb7bb19fae34
YhanMassato <yhanashihara@gmail.com>
2024-02-15 22:42:08 -0300

    Yhan:incialização do estilo do card

--- fundamento-react-01-yhan/src/App.js
+++ fundamento-react-01-yhan/src/App.js
@@ -5,13 +5,18 @@ import cavernas from './livros/cavernas_aco.jpg'
 function App() {
   return (
     <div className="App">
-      <h3>As cavernas de aço</h3>
+      <div className='cardBook'>
+        <h3>As cavernas de aço</h3>
 
-      <p>Isaac Asimov</p>
+        <p>Isaac Asimov</p>
 
-      <img src={cavernas} alt='capa do livro cavernas de aço por Isaac Asimov' style={{width:'300px'}}></img>
+        <img src={cavernas} alt='capa do livro cavernas de aço por Isaac Asimov' style={{width:'800px',height:'300px'}}></img>
 
-      <p>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclusive nos dramas policiais.</p>
+        <div className='descricao-caixa'>
+          <p className='descricao'>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclsive nos dramas policiais.</p>
+        </div>
+
+      </div>
     </div>
   );
 }
