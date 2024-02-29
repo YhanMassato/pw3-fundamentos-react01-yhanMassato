3e91a9f709b2f0a2645ae61be6c3a628a575fa59
YhanMassato <yhanashihara@gmail.com>
2024-02-29 20:24:28 -0300

    renderização dinâmica

--- fundamento-react-01-yhan/src/App.js
+++ fundamento-react-01-yhan/src/App.js
@@ -1,40 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import dadosJSON from "./dados.json"
 import Livro from './components/Livro';
-
-import robos from './livros/robos_imperio.jpg';
-import sol from './livros/sol_desvelado.jpg';
-import cavernas from './livros/cavernas_aco.jpg';
-
-var desc = [ "As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclsive nos dramas policiais.","A obra foi publicada, pela primeira vez, na revista Galaxy Science Fiction, e o sucesso foi tão grande que, um ano depois, se tornou livro. Na trama, Os Mundos Siderais, antigas colônias da Terra, possuem rígidas leis de imigração, e não aceitam mais terráqueos em seus planetas", "Para agravar a situação, os robôs estão substituindo grande parte da mão de obra trabalhadora na Terra, levando os humanos a perderem seus empregos e privilégios de cidadãos" ]
 
 function App() {
 
 
   return (
     <div className="App">
-
       <Livro
-      titulo="As Cavernas de Azimoth"
-      autor="Isaac Azimovit"
-      imagem={cavernas}
-      descricao={desc[0]}
-      /> {/*--Livro1*/}
-
-      <Livro
-      titulo="Sol desolado"
-      autor="Isaac Azimovit"
-      imagem={sol}
-      descricao={desc[1]}
-      /> {/*--Livro2*/}
-
-      <Livro
-      titulo="Robos e Imperio"
-      autor="Isaac Azimovit"
-      imagem={robos}
-      descricao={desc[2]}
-      /> {/*--Livro3*/}
-
+        dadosJSON = {dadosJSON}
+      />{/*--Livro1*/}
     </div> /* --App-- */
   );
 }
--- fundamento-react-01-yhan/src/components/Livro.js
+++ fundamento-react-01-yhan/src/components/Livro.js
@@ -1,15 +1,21 @@
 
-
-function Livro({titulo, autor, imagem, descricao}) {
+function Livro({dadosJSON}) {
     return(
-        <div className='cardBook'>
-            <h3 className="cardTitle">{titulo}</h3>
-            <p>{autor}</p>
-            <img src={imagem} alt='capa do livro cavernas de aço por Isaac Asimov' style={{width:'800px',height:'300px'}}></img>
-            <div className='descricao-caixa'>
-                <p className='descricao'>{descricao}</p>
-            </div>
-        </div> /*--cardBook--*/
+            <>
+                {
+                    dadosJSON.map((livro) => (
+                        <div className='cardBook'>
+                            <h3 className="cardTitle">{livro.titulo}</h3>
+                            <p>{livro.autor}</p>
+                            <img src={livro.imagem} alt='capa do livro cavernas de aço por Isaac Asimov' style={{width:'300px'}}></img>
+                            
+                            <div className='descricao-caixa'>
+                                <p className='descricao'>{livro.descricao}</p>
+                            </div>
+                        </div> /*--cardBook--*/
+                    ))
+                }
+            </>
     );
 }
 
